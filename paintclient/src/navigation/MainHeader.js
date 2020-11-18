b5e3037819b564cff8c16a9863844d8a599aed55
Osiris <potterosiris@gmail.com>
2020-11-17 22:24:45 -0500

    main header js

--- paintclient/src/navigation/MainHeader.js
+++ paintclient/src/navigation/MainHeader.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-const MainHeader = () => {
-  return <div> hi </div>;
+const MainHeader = (props) => {
+  return <header className="main-header">{props.children}</header>;
 };
 
 export default MainHeader;
