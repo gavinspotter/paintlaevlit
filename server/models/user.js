b8aa3aff0331d07a09b71a5f31e903790249370f
Osiris <potterosiris@gmail.com>
2020-11-19 19:39:35 -0500

    user export

--- server/models/user.js
+++ server/models/user.js
@@ -2,9 +2,11 @@ const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
-const userScema = new Schema({
+const userSchema = new Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true, minlength: 6 },
   image: { type: String, required: true },
 });
+
+module.exports = mongoose.model("User", userSchema);
