e09098b6223d72c8c61f71721264ea4c62271d0f
Osiris <potterosiris@gmail.com>
2020-11-19 19:45:21 -0500

    signup try catch

--- server/controllers/users-controller.js
+++ server/controllers/users-controller.js
@@ -1,4 +1,17 @@
 const HttpError = require("../models/HttpError");
 const User = require("../models/user");
 
-const signup = async (req, res, next) => {};
+const signup = async (req, res, next) => {
+  const { name, email, password } = req.body;
+
+  let existingUser;
+  try {
+    existingUser = User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError(
+      "signing up failed please try again later",
+      500
+    );
+    return next(error);
+  }
+};
