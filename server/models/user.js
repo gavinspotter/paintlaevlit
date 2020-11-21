d97748f7f7d58181505ed14e63702b7eff91c32c
Osiris <potterosiris@gmail.com>
2020-11-20 21:02:34 -0500

    unique added to schema

--- server/models/user.js
+++ server/models/user.js
@@ -4,9 +4,9 @@ const uniqueValidator = require("mongoose-unique-validator");
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema({
-  username: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
   name: { type: String, required: true },
-  email: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
   password: { type: String, required: true, minlength: 6 },
   image: { type: String, required: true },
 });
