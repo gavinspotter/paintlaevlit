c5d0070476d88ac4e6ba30c2b5c0c47640067554
Osiris <potterosiris@gmail.com>
2020-11-20 20:37:14 -0500

    login user

--- server/controllers/users-controller.js
+++ server/controllers/users-controller.js
@@ -32,4 +32,26 @@ const signup = async (req, res, next) => {
   res.status(201).json({ user: createdUser.toObject({ getters: true }) });
 };
 
+const login = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  let existingUser;
+  try {
+    existingUser = await User.findOne({ email: email });
+  } catch (err) {
+    const error = new HttpError("login in failed please try again later", 500);
+    return next(error);
+  }
+
+  if (!existingUser || existingUser.password !== password) {
+    const error = new HttpError(
+      "Invalid credentials, could not log you in.",
+      401
+    );
+    return next(error);
+  }
+
+  res.json({ message: "logged in" });
+};
+
 exports.signup = signup;
