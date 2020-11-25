d699493b8edf1250278e8d4f97ff53e99b20c336
Osiris <potterosiris@gmail.com>
2020-11-25 17:56:03 -0500

    send place const

--- server/controllers/foreignpaints-controller.js
+++ server/controllers/foreignpaints-controller.js
@@ -5,3 +5,5 @@ const HttpError = require("../models/HttpError");
 const Foreignpaint = require("../models/foreignpaint");
 
 const User = require("../models/user");
+
+const sendPlace = async (req, res, next) => {};
