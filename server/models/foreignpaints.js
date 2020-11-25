d547a768f3776a8ca0a8765187c6f14379a52d37
Osiris <potterosiris@gmail.com>
2020-11-24 20:13:01 -0500

    foreign paint schema

--- server/models/foreignpaints.js
+++ server/models/foreignpaints.js
@@ -1,3 +1,17 @@
 const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
+
+const foreignpaintSchema = new Schema({
+  room: { type: String, trim: true },
+  roomdimensions: {
+    lenght: { type: Number },
+    width: { type: Number },
+  },
+  paintname: { type: String, trim: true },
+  paintcode: { type: String, trim: true },
+  paintbrand: { type: String, trim: true },
+  creator: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
+});
+
+module.exports = mongoose.model("Foreignpaints", foreignpaintSchema);
