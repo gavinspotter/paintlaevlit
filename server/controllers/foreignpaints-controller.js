fd96e0c11d2880dce6b6e9a9c388c896bcd0a405
Osiris <potterosiris@gmail.com>
2020-11-25 18:00:36 -0500

    obj destructure / const

--- server/controllers/foreignpaints-controller.js
+++ server/controllers/foreignpaints-controller.js
@@ -6,6 +6,27 @@ const Foreignpaint = require("../models/foreignpaint");
 
 const User = require("../models/user");
 
-const sendPlace = async (req, res, next) => {};
+const sendPlace = async (req, res, next) => {
+  const {
+    room,
+    roomdimensions,
+    paintname,
+    paintcode,
+    paintbrand,
+    storecode,
+    reciever,
+    creator,
+  } = req.body;
+
+  const createdPaint = new Foreignpaint({
+    room,
+    roomdimensions,
+    paintname,
+    paintcode,
+    paintbrand,
+    storecode,
+    reciever,
+  });
+};
 
 exports.sendPlace = sendPlace;
