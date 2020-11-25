ec6b1b85077d2b52e5936af955e7fcaff25666a3
Osiris <potterosiris@gmail.com>
2020-11-24 20:29:03 -0500

    foreign paints schema fixed up

--- server/models/foreignpaints.js
+++ server/models/foreignpaints.js
@@ -8,9 +8,10 @@ const foreignpaintSchema = new Schema({
     lenght: { type: Number },
     width: { type: Number },
   },
-  paintname: { type: String, trim: true },
+  paintname: { type: String, trim: true, required: true },
   paintcode: { type: String, trim: true },
   paintbrand: { type: String, trim: true },
+  storecode: { type: String, trim: true },
   creator: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
 });
 
