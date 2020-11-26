17d6096ad6914aa790166a56e704c8df718f2d01
Osiris <potterosiris@gmail.com>
2020-11-26 14:58:03 -0500

    send paints schema

--- server/models/sendpaints.js
+++ server/models/sendpaints.js
@@ -1,3 +1,17 @@
 const mongoose = require("mongoose")
 
 const Schema = mongoose.Schema
+
+const sendpaintsSchema = new Schema({
+    room: { type: String, trim: true, required: true },
+  roomdimensions: {
+    length: {type: Number},
+    width: {type: Number}
+  },
+  paintname: { type: String, trim: true, required: true },
+  paintcode: { type: String, trim: true, required: true },
+  paintbrand: { type: String, trim: true, required: true },
+  storecode: { type: String, trim: true, required: true },
+  sender: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
+
+})
