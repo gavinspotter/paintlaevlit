7b1c9747b208fca4160639fbb846a66403e9b5e0
Osiris <potterosiris@gmail.com>
2020-11-26 15:18:00 -0500

    receive paints schema

--- server/models/receivepaints.js
+++ server/models/receivepaints.js
@@ -1,3 +1,17 @@
 const mongoose = require("mongoose")
 
 const Schema = mongoose.Schema
+
+const receivepaintsSchema = new Schema ({
+    room: { type: String, trim: true, required: true },
+  roomdimensions: {
+    length: {type: Number},
+    width: {type: Number}
+  },
+  paintname: { type: String, trim: true, required: true },
+  paintcode: { type: String, trim: true, required: true },
+  paintbrand: { type: String, trim: true, required: true },
+  storecode: { type: String, trim: true, required: true },
+  receiver: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
+
+})
