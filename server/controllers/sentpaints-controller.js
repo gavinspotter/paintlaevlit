12f5cceab306acf30bb887fc71da955f64e4f256
Osiris <potterosiris@gmail.com>
2020-11-30 20:30:40 -0500

    try catch

--- server/controllers/sentpaints-controller.js
+++ server/controllers/sentpaints-controller.js
@@ -8,8 +8,13 @@ const getSentPaintsByUserId = async (req, res, next) => {
     const userId = req.params.uid
 
     let sentpaint 
-    
-  
+
+  try {
+      sentpaint = await Sentpaint.find({sender: userId})
+  } catch (err) {
+      const error = new HttpError(" fetching sent paint failed", 500)
+      return next(error)
+  }
 
 }
 
