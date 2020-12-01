ee86228cffee2a0d1acacca9a09e57c09e5f0ea2
Osiris <potterosiris@gmail.com>
2020-11-30 20:23:37 -0500

    imp sent paints

--- server/controllers/sentpaints-controller.js
+++ server/controllers/sentpaints-controller.js
@@ -2,4 +2,4 @@ const mongoose = require("mongoose")
 
 const HttpError = require("../models/HttpError")
 
-const Paint = require("../models/paints")
+const Sentpaint = require("../models/sendpaints")
