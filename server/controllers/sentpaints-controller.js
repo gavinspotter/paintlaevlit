98539e24ff682d09f267e986c0ec44d7e07e5277
Osiris <potterosiris@gmail.com>
2020-11-30 20:25:13 -0500

    func

--- server/controllers/sentpaints-controller.js
+++ server/controllers/sentpaints-controller.js
@@ -3,3 +3,7 @@ const mongoose = require("mongoose")
 const HttpError = require("../models/HttpError")
 
 const Sentpaint = require("../models/sendpaints")
+
+const getSentPaintsByUserId = async (req, res, next) => {
+    
+}
