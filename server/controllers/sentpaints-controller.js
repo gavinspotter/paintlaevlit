84fdcf94ae20c6b33558d8276076050fb96bf4e0
Osiris <potterosiris@gmail.com>
2020-11-30 20:27:17 -0500

    params

--- server/controllers/sentpaints-controller.js
+++ server/controllers/sentpaints-controller.js
@@ -5,6 +5,9 @@ const HttpError = require("../models/HttpError")
 const Sentpaint = require("../models/sendpaints")
 
 const getSentPaintsByUserId = async (req, res, next) => {
+    const userId = req.params.uid
+
+  
 
 }
 
