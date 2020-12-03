7c34a223dd22e4a7c4de908c5987e1cec0c697aa
Osiris <potterosiris@gmail.com>
2020-12-02 19:11:31 -0500

    const user id

--- server/controllers/receivedpaints-controller.js
+++ server/controllers/receivedpaints-controller.js
@@ -3,7 +3,7 @@ const HttpError = require("../models/HttpError")
 const Receivedpaint = require("../models/receivepaints")
 
 const getReceivedPaintsByUserId = async (req, res, next) => {
-
+    const userId = req.params.uid 
 }
 
 exports.getReceivedPaintsByUserId = getReceivedPaintsByUserId
