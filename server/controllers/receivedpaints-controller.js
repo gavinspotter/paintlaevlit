c06ba70a3be82273a8b38459ac60b28e1d6bd9cd
Osiris <potterosiris@gmail.com>
2020-12-02 19:14:08 -0500

    try catch

--- server/controllers/receivedpaints-controller.js
+++ server/controllers/receivedpaints-controller.js
@@ -6,6 +6,16 @@ const getReceivedPaintsByUserId = async (req, res, next) => {
     const userId = req.params.uid 
 
     let receivedpaint 
+
+    try {
+        receivedpaint = await Receivedpaint.find({sender:userId})
+    } catch (err) {
+        const error = new HttpError("fetching received paint failed", 500)
+        return next(error)
+    }
+
+    
+
 }
 
 exports.getReceivedPaintsByUserId = getReceivedPaintsByUserId
