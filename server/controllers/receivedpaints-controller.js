91a9cbafc67e4a182c1e5851f57c218fbe07c3b6
Osiris <potterosiris@gmail.com>
2020-12-02 19:09:15 -0500

    func

--- server/controllers/receivedpaints-controller.js
+++ server/controllers/receivedpaints-controller.js
@@ -1,3 +1,7 @@
 const HttpError = require("../models/HttpError")
 
 const Receivedpaint = require("../models/receivepaints")
+
+const getReceivedPaintsByUserId = async (req, res, next) => {
+    
+}
