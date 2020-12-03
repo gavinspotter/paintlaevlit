5f2694a9b5b57ec97af736cc276e22f796a7e22a
Osiris <potterosiris@gmail.com>
2020-12-02 19:09:52 -0500

    export

--- server/controllers/receivedpaints-controller.js
+++ server/controllers/receivedpaints-controller.js
@@ -3,5 +3,7 @@ const HttpError = require("../models/HttpError")
 const Receivedpaint = require("../models/receivepaints")
 
 const getReceivedPaintsByUserId = async (req, res, next) => {
-    
+
 }
+
+exports.getReceivedPaintsByUserId = getReceivedPaintsByUserId
