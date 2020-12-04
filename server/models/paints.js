0306721ea5e10241d12ca4cb3f54e8cba318aeee
Osiris <potterosiris@gmail.com>
2020-12-03 22:18:59 -0500

    store code

--- server/models/paints.js
+++ server/models/paints.js
@@ -11,7 +11,7 @@ const paintSchema = new Schema({
   paintname: { type: String, trim: true, required: true },
   paintcode: { type: String, trim: true, required: true },
   paintbrand: { type: String, trim: true, required: true },
-  storecode: { type: String, trim: true, required: true },
+  storecode: { type: String, trim: true},
   creator: { type: mongoose.Types.ObjectId, required: true, ref: "User" },
 });
 
