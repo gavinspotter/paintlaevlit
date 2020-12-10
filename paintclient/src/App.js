f66e3724d837cfb67632d1270f3f606142986059
Osiris <potterosiris@gmail.com>
2020-12-09 20:58:30 -0500

    main header css

--- paintclient/src/App.js
+++ paintclient/src/App.js
@@ -1,10 +1,10 @@
-import logo from "./logo.svg";
+
 import "./App.css";
 
-import MainHeader from "./navigation/MainHeader";
+
 
 function App() {
-  return <MainHeader> hi </MainHeader>;
+  
 }
 
 export default App;
--- paintclient/src/navigation/MainNavigation.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-import NavLinks from "./NavLinks";
-
-const MainNavigation = (props) => {};
-
-export default MainNavigation;
