e41b8d416c258b273bf8bd67c48cef863d982f82
Osiris <potterosiris@gmail.com>
2020-12-14 19:41:43 -0500

    auth const

--- paintclient/src/shared/components/navigation/NavLinks.js
+++ paintclient/src/shared/components/navigation/NavLinks.js
@@ -1,9 +1,11 @@
-import React from "react"
+import React, { useContext } from "react"
 import {NavLink} from "react-router-dom"
 
 import {AuthContext} from "../../context/auth-context"
 import "./NavLinks.css"
 
-const NavLinks = () => {}
+const NavLinks = () => {
+    const auth = useContext(AuthContext)
+}
 
 export default NavLinks
