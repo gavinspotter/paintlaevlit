1b88d76bd0223e4ffae4e19d1feb70a63c2f8134
Osiris <potterosiris@gmail.com>
2020-12-16 15:30:28 -0500

    card login

--- paintclient/src/user/pages/Auth.js
+++ paintclient/src/user/pages/Auth.js
@@ -1,9 +1,18 @@
 import React, { useContext } from "react"
+import Card from "../../shared/components/UIElements/Card"
 
 import {AuthContext} from "../../shared/context/auth-context"
 
 const Auth = () => {
     const auth = useContext(AuthContext)
+
+    return (
+        <Card className="authentication">
+            <h2>login</h2>
+            
+        </Card>
+    )
+
 }
 
 export default Auth
