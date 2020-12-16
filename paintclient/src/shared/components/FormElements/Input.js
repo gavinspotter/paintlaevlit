225d0097d14dbcacdf1dd2c83636fbc929b52ba4
Osiris <potterosiris@gmail.com>
2020-12-16 15:20:33 -0500

    input

--- paintclient/src/shared/components/FormElements/Input.js
+++ paintclient/src/shared/components/FormElements/Input.js
@@ -1,5 +1,29 @@
 import React from "react"
 
-const Input = () => {}
+const Input = (props) => {
+
+    const element = props.element === "input" ? (
+        <input 
+        name={props.name}
+        ref={props.valRef}
+        />
+    ) : (
+        <textarea
+        rows={props.rows || 3}
+        ref={props.valRef}
+        name={props.name}
+        />
+    )
+
+    return (
+        <div>
+            <label>
+                {props.label}
+            </label> <br/>
+            {element}
+
+        </div>
+    )
+}
 
 export default Input
