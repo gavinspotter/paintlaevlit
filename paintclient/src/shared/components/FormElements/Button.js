cdb529579ffa80542531afe9257d0b0b47741fc4
Osiris <potterosiris@gmail.com>
2020-12-17 20:35:33 -0500

    button

--- paintclient/src/shared/components/FormElements/Button.js
+++ paintclient/src/shared/components/FormElements/Button.js
@@ -17,6 +17,18 @@ const Button = (props) => {
           </Link>
         );
       }
+
+      return (
+        <button
+          className={`button button--${props.size || 'default'} ${props.inverse &&
+            'button--inverse'} ${props.danger && 'button--danger'}`}
+          type={props.type}
+          onClick={props.onClick}
+          disabled={props.disabled}
+        >
+          {props.children}
+        </button>
+      );
 }
 
 export default Button
