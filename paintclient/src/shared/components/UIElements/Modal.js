0badb0f65153442d48c53b3e0a07723759db4135
Osiris <potterosiris@gmail.com>
2020-12-17 21:10:59 -0500

    content const

--- paintclient/src/shared/components/UIElements/Modal.js
+++ paintclient/src/shared/components/UIElements/Modal.js
@@ -3,4 +3,24 @@ import ReactDOM from "react-dom"
 
 import "./Modal.css"
 
-const ModalOverlay = (props) => {}
+const ModalOverlay = (props) => {
+    const content = (
+        <div className={`modal ${props.className}`} style={props.style}>
+          <header className={`modal__header ${props.headerClass}`}>
+            <h2>{props.header}</h2>
+          </header>
+          <form
+            onSubmit={
+              props.onSubmit ? props.onSubmit : (event) => event.preventDefault()
+            }
+          >
+            <div className={`modal__content ${props.contentClass}`}>
+              {props.children}
+            </div>
+            <footer className={`modal__footer ${props.footerClass}`}>
+              {props.footer}
+            </footer>
+          </form>
+        </div>
+      );
+}
