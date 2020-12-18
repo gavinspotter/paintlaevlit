adfdec452b70b0e742a58b3b6d3ac7f9d5ca2fda
Osiris <potterosiris@gmail.com>
2020-12-17 20:32:54 -0500

    link if state

--- paintclient/src/shared/components/FormElements/Button.js
+++ paintclient/src/shared/components/FormElements/Button.js
@@ -4,8 +4,19 @@ import {Link} from "react-router-dom"
 import "./Button.css"
 
 
-const Button = () => {
-    
+const Button = (props) => {
+    if (props.to) {
+        return (
+          <Link
+            to={props.to}
+            exact={props.exact}
+            className={`button button--${props.size || 'default'} ${props.inverse &&
+              'button--inverse'} ${props.danger && 'button--danger'}`}
+          >
+            {props.children}
+          </Link>
+        );
+      }
 }
 
 export default Button
