66595c1a0345e27e50351b1715c4b131926d6140
Osiris <potterosiris@gmail.com>
2020-12-19 13:58:55 -0500

    error modal

--- paintclient/src/shared/components/UIElements/ErrorModal.js
+++ paintclient/src/shared/components/UIElements/ErrorModal.js
@@ -1,9 +1,20 @@
 import React from "react"
+import Button from "../FormElements/Button"
 
 import Modal from "./Modal"
 
 const ErrorModal = props => {
 
+    return (
+        <Modal
+        onCancel={props.onClear}
+      header="An Error Occurred!"
+      show={!!props.error}
+      footer={<Button onClick={props.onClear}> okay </Button>}
+        >
+            
+        </Modal>
+    )
 }
 
 export default ErrorModal
