469161616955ec494bed0a0def80aa52abde4c85
Osiris <potterosiris@gmail.com>
2020-12-19 19:28:09 -0500

    http hook

--- paintclient/src/user/pages/Signup.js
+++ paintclient/src/user/pages/Signup.js
@@ -2,6 +2,7 @@ import React from "react"
 import { useForm } from "react-hook-form"
 
 import { AuthContext } from "../../shared/context/auth-context"
+import { useHttpClient } from "../../shared/hooks/http-hook"
 
 
 const Signup = () => {
