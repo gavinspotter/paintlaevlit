5e8b973ca4d22da0b8d3b550cc1052a44ad5fadd
Osiris <potterosiris@gmail.com>
2020-12-20 19:02:46 -0500

    auth const

--- paintclient/src/paints/pages/NewPaint.js
+++ paintclient/src/paints/pages/NewPaint.js
@@ -1,9 +1,9 @@
-import React from "react"
+import React, { useContext } from "react"
 import { useHistory } from "react-router-dom"
 import { AuthContext } from "../../shared/context/auth-context"
 
 const NewPaint = () => {
-
+    const auth = useContext(AuthContext)
 }
 
 export default NewPaint
