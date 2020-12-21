ae32ed95e38423970680a4026aed7aba3e53017c
Osiris <potterosiris@gmail.com>
2020-12-20 19:30:09 -0500

    loading spinner

--- paintclient/src/paints/pages/NewPaint.js
+++ paintclient/src/paints/pages/NewPaint.js
@@ -3,6 +3,7 @@ import { useForm } from "react-hook-form"
 import { useHistory } from "react-router-dom"
 import Card from "../../shared/components/UIElements/Card"
 import ErrorModal from "../../shared/components/UIElements/ErrorModal"
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner"
 import { AuthContext } from "../../shared/context/auth-context"
 import { useHttpClient } from "../../shared/hooks/http-hook"
 
@@ -43,7 +44,7 @@ const NewPaint = () => {
             <ErrorModal error={error} onClear={clearError} />
             <Card>
                 <form onSubmit={handleSubmit(onSubmit)}>
-
+                    {isLoading && <LoadingSpinner asOverlay />}
                 </form>
             </Card>
         </React.Fragment>
