28b07add019274a5fb7ca063fbc5460766605b26
Osiris <potterosiris@gmail.com>
2020-12-20 19:04:06 -0500

    http client imp

--- paintclient/src/paints/pages/NewPaint.js
+++ paintclient/src/paints/pages/NewPaint.js
@@ -1,9 +1,13 @@
 import React, { useContext } from "react"
 import { useHistory } from "react-router-dom"
 import { AuthContext } from "../../shared/context/auth-context"
+import { useHttpClient } from "../../shared/hooks/http-hook"
 
 const NewPaint = () => {
     const auth = useContext(AuthContext)
+
+
+
 }
 
 export default NewPaint
