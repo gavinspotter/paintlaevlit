a1842ca87bfe373fa3e346e860495c437c8dfb6c
Osiris <potterosiris@gmail.com>
2020-12-27 15:12:34 -0500

    use state const

--- paintclient/src/paints/pages/Paint.js
+++ paintclient/src/paints/pages/Paint.js
@@ -1,10 +1,16 @@
-import React from "react"
+import React, { useState } from "react"
 import { useParams } from "react-router-dom"
 
 import { useHttpClient } from "../../shared/hooks/http-hook"
 
 
-const Paint = () => { }
+const Paint = () => {
+
+    const [loadedPaint, setLoadedPaint] = useState()
+
+
+
+}
 
 export default Paint
 
