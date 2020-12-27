da5a92a9ec6e413a2bf16df5e43b151878d03890
Osiris <potterosiris@gmail.com>
2020-12-27 15:11:42 -0500

    http hook imp

--- paintclient/src/paints/pages/Paint.js
+++ paintclient/src/paints/pages/Paint.js
@@ -1,6 +1,9 @@
 import React from "react"
 import { useParams } from "react-router-dom"
 
+import { useHttpClient } from "../../shared/hooks/http-hook"
+
+
 const Paint = () => { }
 
 export default Paint
