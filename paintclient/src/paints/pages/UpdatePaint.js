18eda2cadd79fd38e986b68bbeb12900262aefdc
Osiris <potterosiris@gmail.com>
2020-12-27 23:32:47 -0500

    auth const

--- paintclient/src/paints/pages/UpdatePaint.js
+++ paintclient/src/paints/pages/UpdatePaint.js
@@ -1,11 +1,11 @@
-import React from "react"
+import React, { useContext } from "react"
 
 import { useHttpClient } from "../../shared/hooks/http-hook"
 import { AuthContext } from "../../shared/context/auth-context"
 
 const UpdatePaint = () => {
 
-
+    const auth = useContext(AuthContext)
 
 }
 
