fd1b5653af754810beed37c90ad7b4e309aa24f0
Osiris <potterosiris@gmail.com>
2020-12-27 23:31:32 -0500

    http client

--- paintclient/src/paints/pages/UpdatePaint.js
+++ paintclient/src/paints/pages/UpdatePaint.js
@@ -1,5 +1,6 @@
 import React from "react"
 
+import { useHttpClient } from "../../shared/hooks/http-hook"
 import { AuthContext } from "../../shared/context/auth-context"
 
 const UpdatePaint = () => {
