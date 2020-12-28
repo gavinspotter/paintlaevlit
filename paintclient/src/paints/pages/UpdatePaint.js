85bb0967f50ef4cef083250ae3ada77de5bf1f97
Osiris <potterosiris@gmail.com>
2020-12-27 23:33:43 -0500

    paint id

--- paintclient/src/paints/pages/UpdatePaint.js
+++ paintclient/src/paints/pages/UpdatePaint.js
@@ -2,10 +2,12 @@ import React, { useContext } from "react"
 
 import { useHttpClient } from "../../shared/hooks/http-hook"
 import { AuthContext } from "../../shared/context/auth-context"
+import { useParams } from "react-router-dom"
 
 const UpdatePaint = () => {
 
     const auth = useContext(AuthContext)
+    const paintId = useParams().paintId
 
 }
 
