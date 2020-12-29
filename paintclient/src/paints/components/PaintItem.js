4c64d9bb46502f8057c1332b8e0d0b80467f4599
Osiris <potterosiris@gmail.com>
2020-12-28 21:17:43 -0500

    import

--- paintclient/src/paints/components/PaintItem.js
+++ paintclient/src/paints/components/PaintItem.js
@@ -1,4 +1,4 @@
-import React, { useContext, useState } from "react"
+import React, { useState } from "react"
 import Button from "../../shared/components/FormElements/Button"
 
 import Card from "../../shared/components/UIElements/Card"
