fffd42122d457bb41a9adffb2702c442d6393807
Osiris <potterosiris@gmail.com>
2020-12-28 21:46:41 -0500

    use form

--- paintclient/src/sendpaints/pages/SendPaint.js
+++ paintclient/src/sendpaints/pages/SendPaint.js
@@ -1,4 +1,7 @@
 import React from "react"
+import { useForm } from "react-hook-form"
+import Input from "../../shared/components/FormElements/Input"
+import Card from "../../shared/components/UIElements/Card"
 import ErrorModal from "../../shared/components/UIElements/ErrorModal"
 
 import { useHttpClient } from "../../shared/hooks/http-hook"
@@ -8,11 +11,22 @@ const SendPaint = () => {
 
     const { isLoading, error, sendRequest, clearError } = useHttpClient()
 
+    const { register, handleSubmit } = useForm()
+
 
     return (
         <React.Fragment>
             <ErrorModal error={error} onClear={clearError} />
+            <Card>
+                <form>
+                    <Input />
+                    <Input />
+                    <Input />
+                    <Input />
+                    <Input />
 
+                </form>
+            </Card>
         </React.Fragment>
     )
 
