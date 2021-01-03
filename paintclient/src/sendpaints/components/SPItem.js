4fbdff847d41c1fcf80ebade15e60ab7165fd3f2
Osiris <potterosiris@gmail.com>
2021-01-03 18:34:38 -0500

    hh const

--- paintclient/src/sendpaints/components/SPItem.js
+++ paintclient/src/sendpaints/components/SPItem.js
@@ -1,9 +1,12 @@
 import React from "react"
 
+
+
 import { useHttpClient } from "../../shared/hooks/http-hook"
 
 const SPItem = () => {
 
+    const { isLoading, error, sendRequest, clearError } = useHttpClient()
 
 
 }
