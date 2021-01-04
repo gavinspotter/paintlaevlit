ac849711e488c79c63e798a66fe0fcb9765f7e43
Osiris <potterosiris@gmail.com>
2021-01-03 19:15:16 -0500

    if

--- paintclient/src/sendpaints/components/SPList.js
+++ paintclient/src/sendpaints/components/SPList.js
@@ -1,7 +1,16 @@
 import React from "react"
+import Button from "../../shared/components/FormElements/Button"
 
-const SPList = () => {
+const SPList = (props) => {
 
+    if (props.items.length === 0) {
+        return (
+            <div>
+                <h2> no paints found </h2>
+                <Button to="sendpaints/new"> send paint </Button>
+            </div>
+        )
+    }
 }
 
 export default SPList
