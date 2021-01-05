f41eca2c164a698c52c70078dff74d1aade73c2a
Osiris <potterosiris@gmail.com>
2021-01-04 21:47:21 -0500

    usehttpclient const

--- paintclient/src/sendpaints/pages/SentPaint.js
+++ paintclient/src/sendpaints/pages/SentPaint.js
@@ -6,6 +6,7 @@ const SentPaint = () => {
 
     const [loadedPaint, setLoadedPaint] = useState()
 
+    const { isLoading, error, sendRequest, clearError } = useHttpClient()
 }
 
 export default SentPaint
