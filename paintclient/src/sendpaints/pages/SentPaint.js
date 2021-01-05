fdc0b321936ead01a922e6097364addedcbed9d0
Osiris <potterosiris@gmail.com>
2021-01-04 21:46:16 -0500

    http hook commit

--- paintclient/src/sendpaints/pages/SentPaint.js
+++ paintclient/src/sendpaints/pages/SentPaint.js
@@ -1,5 +1,7 @@
 import React, { useState } from "react"
 
+import { useHttpClient } from "../../shared/hooks/http-hook"
+
 const SentPaint = () => {
 
     const [loadedPaint, setLoadedPaint] = useState()
