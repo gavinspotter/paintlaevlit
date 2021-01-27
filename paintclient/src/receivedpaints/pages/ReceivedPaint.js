57ed8f10650b88d644cd0537701ebd316c599171
Osiris <potterosiris@gmail.com>
2021-01-26 19:52:31 -0500

    loading spinner

--- paintclient/src/receivedpaints/pages/ReceivedPaint.js
+++ paintclient/src/receivedpaints/pages/ReceivedPaint.js
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react"
 import { useParams } from "react-router-dom"
 import ErrorModal from "../../shared/components/UIElements/ErrorModal"
+import LoadingSpinner from "../../shared/components/UIElements/LoadingSpinner"
 
 import { useHttpClient } from "../../shared/hooks/http-hook"
 
@@ -29,6 +30,11 @@ const ReceivedPaint = () => {
     return (
         <React.Fragment>
             <ErrorModal error={error} onClear={clearError} />
+            {isLoading && (
+                <div className="center">
+                    <LoadingSpinner />
+                </div>
+            )}
         </React.Fragment>
     )
 
