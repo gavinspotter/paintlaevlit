1794dd1e6989957af3014313c76edc96ecf22a09
Osiris <potterosiris@gmail.com>
2021-01-26 19:33:12 -0500

    rf

--- paintclient/src/receivedpaints/components/RPItem.js
+++ paintclient/src/receivedpaints/components/RPItem.js
@@ -1,5 +1,12 @@
 import React from "react"
 
-const RPItem = () => { }
+const RPItem = () => {
+
+    return (
+        <React.Fragment>
+
+        </React.Fragment>
+    )
+}
 
 export default RPItem
