2138cde0e06c4d43481e7fe570b8fadd78a807f4
Osiris <potterosiris@gmail.com>
2021-01-26 19:43:30 -0500

    hook const

--- paintclient/src/receivedpaints/pages/ReceivedPaint.js
+++ paintclient/src/receivedpaints/pages/ReceivedPaint.js
@@ -3,6 +3,11 @@ import React from "react"
 import { useHttpClient } from "../../shared/hooks/http-hook"
 
 
-const ReceivedPaint = () => { }
+const ReceivedPaint = () => {
+
+    const { isLoading, error, sendRequest, clearError } = useHttpClient()
+
+
+}
 
 export default ReceivedPaint
