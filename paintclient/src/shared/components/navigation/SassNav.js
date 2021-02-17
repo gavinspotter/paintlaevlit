304bc7553eb557016a4fc1396c9665fafeaab274
Osiris <potterosiris@gmail.com>
2021-02-17 12:21:46 -0500

    to

--- paintclient/src/shared/components/navigation/SassNav.js
+++ paintclient/src/shared/components/navigation/SassNav.js
@@ -15,11 +15,11 @@ const SassNav = () => {
             <div className="navigation__background"></div>
             <nav className="navigation__nav">
                 <ul className="navigation__list">
-                    <li className="navigation__item"><Link to="" className="navigation__link">saved paint</Link></li>
-                    <li className="navigation__item"><Link to="" className="navigation__link">create paint</Link></li>
-                    <li className="navigation__item"><Link to="" className="navigation__link">send paint</Link></li>
-                    <li className="navigation__item"><Link to="" className="navigation__link">sent paint</Link></li>
-                    <li className="navigation__item"><Link to="" className="navigation__link">received paint</Link></li>
+                    <li className="navigation__item"><Link to={`/${auth.userId}/paints`} className="navigation__link">saved paint</Link></li>
+                    <li className="navigation__item"><Link to="/paints/new" className="navigation__link">create paint</Link></li>
+                    <li className="navigation__item"><Link to={`/${auth.userId}/sent`} className="navigation__link">send paint</Link></li>
+                    <li className="navigation__item"><Link to="/send/new" className="navigation__link">sent paint</Link></li>
+                    <li className="navigation__item"><Link to={`/${auth.userId}/received`} className="navigation__link">received paint</Link></li>
                 </ul>
             </nav>
 
