e5e30112f093e05224331d12e503015646e9a922
Osiris <potterosiris@gmail.com>
2021-02-17 08:11:17 -0500

    sass nav

--- paintclient/src/shared/components/navigation/SassNav.js
+++ paintclient/src/shared/components/navigation/SassNav.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 const SassNav = () => {
     return (
-        <div>
+        <div className="navigation">
+            <input type="checkbox" className="navigation__checkbox" id="navi-toggle" />
+            <label for="navi-toggle" className="navigation__button"><span className="navigation__icon"></span></label>
+            <div className="navigation__background"></div>
+            <nav className="navigation__nav">
+                <ul className="navigation__list">
+                    <li className="navigation__item"></li>
+                    <li className="navigation__item"></li>
+                    <li className="navigation__item"></li>
+                    <li className="navigation__item"></li>
+                </ul>
+            </nav>
 
         </div>
     )
