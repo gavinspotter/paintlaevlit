c63e9eefdf6ea4f4a3636562e994342175b42c92
Osiris <potterosiris@gmail.com>
2021-02-22 18:10:48 -0500

    error modal

--- paintclient/src/receivedpaints/components/RPItem.js
+++ paintclient/src/receivedpaints/components/RPItem.js
@@ -1,30 +1,35 @@
 import React from "react"
 import Card from "../../shared/components/UIElements/Card"
+import ErrorModal from "../../shared/components/UIElements/ErrorModal"
 
 const RPItem = (props) => {
 
     return (
-        <li>
-            <div className="receivedpaint__card">
-                <div className="receivedpaint__card-text">
-                    <div>
-                        {props.paintname}
-                    </div>
-                    <div>
-                        {props.paintcode}
-                    </div>
-                    <div>
-                        {props.paintbrand}
-                    </div>
-                    <div>
-                        {props.base}
-                    </div>
-                    <div>
-                        {props.storecode}
+
+        <React.Fragment>
+            <ErrorModal />
+            <li>
+                <div className="receivedpaint__card">
+                    <div className="receivedpaint__card-text">
+                        <div>
+                            {props.paintname}
+                        </div>
+                        <div>
+                            {props.paintcode}
+                        </div>
+                        <div>
+                            {props.paintbrand}
+                        </div>
+                        <div>
+                            {props.base}
+                        </div>
+                        <div>
+                            {props.storecode}
+                        </div>
                     </div>
                 </div>
-            </div>
-        </li>
+            </li>
+        </React.Fragment>
     )
 }
 
