ebf7b042467b633e0422f122f1b7b2f219b978c4
Osiris <potterosiris@gmail.com>
2021-03-01 19:42:27 -0500

    password

--- server/controllers/users-controller.js
+++ server/controllers/users-controller.js
@@ -102,7 +102,7 @@ const login = async (req, res, next) => {
     return next(error);
   }
 
-  if (!existingUser || existingUser.password !== password) {
+  if (!existingUser) {
     const error = new HttpError(
       "Invalid credentials, could not log you in.",
       401
