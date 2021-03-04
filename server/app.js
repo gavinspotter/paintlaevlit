6f8b5af46a897bbb2dbf00def55c728e7e82cac9
Osiris <potterosiris@gmail.com>
2021-03-04 17:42:06 -0500

    process. env

--- server/app.js
+++ server/app.js
@@ -52,7 +52,7 @@ app.use((error, req, res, next) => {
 
 mongoose
   .connect(
-    "mongodb+srv://gavin:Password123@cluster0.d3tnt.mongodb.net/paintstuff?retryWrites=true&w=majority"
+    `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.d3tnt.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`
   )
   .then(() => {
     app.listen(5000);
